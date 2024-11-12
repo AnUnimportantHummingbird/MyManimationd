fb02a570a9db6968cc175c2cbd3121e2a0bc620e
A Person <ai6543r@gmail.com>
2024-11-11 20:30:05 -0500

    Finished Diagram
    After an hour of trying I figured it out

--- ForVideos/2024/AGeometryProblem/main.py
+++ ForVideos/2024/AGeometryProblem/main.py
@@ -11,13 +11,17 @@ class ThumbNail(Scene):
         A3 = ArcBetweenPoints(s.get_vertices()[2], s.get_vertices()[0])
         A4 = ArcBetweenPoints(s.get_vertices()[3], s.get_vertices()[1])
 
-        # A point on each line
-        A1point = Point(A1.get_bottom())
+        # Length of a side of a square
+        side = Line(s.get_vertices()[0], s.get_vertices()[1])
 
         # A Circle that makes the arcs mobjects
-        A1mob = Circle.from_three_points(s.get_vertices()[0], s.get_vertices()[2], A1.get_top())
+        A1mob = Circle(radius = side.get_length()).move_to(s.get_vertices()[0])
+        A2mob = Circle(radius = side.get_length()).move_to(s.get_vertices()[1])
+        A3mob = Circle(radius = side.get_length()).move_to(s.get_vertices()[2])
+        A4mob = Circle(radius = side.get_length()).move_to(s.get_vertices()[3])
 
         # Shaded Area
+        ShadedArea = Intersection(A1mob, A2mob, A3mob, A4mob, color = WHITE, fill_opacity = 0.5)
 
-
-        self.add(VGroup(s, A1, A2, A3, A4, A1mob))
+        self.add(VGroup(s, A1, A2, A3, A4))
+        self.add(ShadedArea)
