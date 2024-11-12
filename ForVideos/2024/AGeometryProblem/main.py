156fe483fcece9bc5da5d810dd059b6667b7a988
A Person <ai6543r@gmail.com>
2024-11-11 19:44:33 -0500

    asdf

--- ForVideos/2024/AGeometryProblem/main.py
+++ ForVideos/2024/AGeometryProblem/main.py
@@ -3,12 +3,6 @@ from manim import *
 # The Thumbnail
 class ThumbNail(Scene):
     def construct(self):
-        s = Square().scale(2)
-        side = Line(s.get_vertices()[1], s.get_vertices()[2])
-        SideLength = Tex("4").move_to(2.25*LEFT)
-        q = ArcBetweenPoints(s.get_vertices()[3], s.get_vertices()[1])
-        
+        s = Square().scale(2.5)
 
-        self.add(s)
-        self.add(q)
-        self.add(SideLength)
+        self.add(VGroup(s))
