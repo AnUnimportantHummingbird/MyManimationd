4fb8d54056ae0c444fb9a041994d276d8d0c8c23
A Person <ai6543r@gmail.com>
2024-11-11 20:03:00 -0500

    12

--- ForVideos/2024/AGeometryProblem/main.py
+++ ForVideos/2024/AGeometryProblem/main.py
@@ -5,4 +5,19 @@ class ThumbNail(Scene):
     def construct(self):
         s = Square().scale(2.5)
 
-        self.add(VGroup(s))
+        # Quarter Circles
+        A1 = ArcBetweenPoints(s.get_vertices()[0], s.get_vertices()[2])
+        A2 = ArcBetweenPoints(s.get_vertices()[1], s.get_vertices()[3])
+        A3 = ArcBetweenPoints(s.get_vertices()[2], s.get_vertices()[0])
+        A4 = ArcBetweenPoints(s.get_vertices()[3], s.get_vertices()[1])
+
+        # A point on each line
+        A1point = Point(A1.get_bottom())
+
+        # A Circle that makes the arcs mobjects
+        A1mob = Circle.from_three_points(s.get_vertices()[0], s.get_vertices()[2], A1.get_top())
+
+        # Shaded Area
+
+
+        self.add(VGroup(s, A1, A2, A3, A4, A1mob))
