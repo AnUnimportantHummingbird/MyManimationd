50f157315de97e467e420f34d9f62da8a3b3597d
A Person <ai6543r@gmail.com>
2024-11-12 17:32:05 -0500

    Hihihihih

--- ForVideos/2024/AGeometryProblem/main.py
+++ ForVideos/2024/AGeometryProblem/main.py
@@ -11,6 +11,13 @@ class ThumbNail(Scene):
         A3 = ArcBetweenPoints(s.get_vertices()[2], s.get_vertices()[0])
         A4 = ArcBetweenPoints(s.get_vertices()[3], s.get_vertices()[1])
 
+        # Makes a point on each of the verticies
+        A = Point(s.get_vertices()[0])
+        B = Point(s.get_vertices()[1])
+        C = Point(s.get_vertices()[2])
+        D = Point(s.get_vertices()[3])
+        
+
         # Length of a side of a square
         side = Line(s.get_vertices()[0], s.get_vertices()[1])
 
@@ -23,5 +30,8 @@ class ThumbNail(Scene):
         # Shaded Area
         ShadedArea = Intersection(A1mob, A2mob, A3mob, A4mob, color = WHITE, fill_opacity = 0.5)
 
+        # The question
+        q = Tex("Can you solve this?").to_edge(UP, buff=0.5)
+
         self.add(VGroup(s, A1, A2, A3, A4))
-        self.add(ShadedArea)
+        self.add(ShadedArea, q, A)
